3d139ebcc48c19fc6c500696922c47a23213a492
Алексей Уханов <dropdatabase.py@gmail.com>
2024-04-13 11:12:26 +0300

    Add route to get banners with options

--- banners_app/orm/repository.py
+++ banners_app/orm/repository.py
@@ -8,7 +8,7 @@ from ..schemas.banner_schemas import *
 
 class BannerRepository:
     @staticmethod
-    async def add_one(data: SBannerAdd):
+    async def add_banner(data: SBannerAdd):
         async with new_session() as session:
 
             query = (
@@ -28,3 +28,27 @@ class BannerRepository:
             banner_id = banner.banner_id
             await session.commit()
             return banner_id
+
+    @staticmethod
+    async def get_banners(banner_options) -> list[SBanner]:
+        async with new_session() as session:
+            query = select(BannersOrm)
+
+            if banner_options.feature_id is not None:
+                query = query.filter(
+                    BannersOrm.feature_id == banner_options.feature_id)
+            if banner_options.tag_id is not None:
+                query = query.where(
+                    BannersOrm.tag_ids.any(banner_options.tag_id))
+            if banner_options.offset is not None:
+                query = query.offset(banner_options.offset)
+            if banner_options.limit is not None:
+                query = query.limit(banner_options.limit)
+
+            result = await session.execute(query)
+            banner_models = result.scalars().all()
+
+            banner_schemas = [SBanner.model_validate(
+                banner_orm, from_attributes=True) for banner_orm in banner_models]
+
+            return banner_schemas
--- banners_app/routers/banner_router.py
+++ banners_app/routers/banner_router.py
@@ -4,7 +4,22 @@ banner_router = APIRouter(prefix='/banner', tags=["user_banner"])
 
 
 @banner_router.post('/')
-async def add_banner(banner: SBannerAdd, token: Annotated[str | None, Header()] = None) -> SBannerAdded:
+async def add_banner(
+    banner: SBannerAdd,
+    token: Annotated[str | None, Header()] = None
+) -> SBannerAdded:
+
     verify_token(token=token)
-    banner_id = await BannerRepository.add_one(banner)
+    banner_id = await BannerRepository.add_banner(banner)
     return JSONResponse(status_code=201, content={'banner_id': banner_id})
+
+
+@banner_router.get('/', status_code=200)
+async def get_banner(
+    banner_options: Annotated[SBannerGet, Depends()],
+    token: str | None = Header(default=None)
+) -> list[SBanner]:
+
+    verify_token(token=token)
+    banners = await BannerRepository.get_banners(banner_options=banner_options)
+    return banners
